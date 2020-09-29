da776d8072aa47aff99bb5711fefa94e0750814e
Erick Castille <ecastille924@gmail.com>
2020-09-29 23:05:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,6 +41,20 @@ WIN_COMBINATIONS = [
   end
   
   def turn 
+     puts "Player #{current_player}, please enter a number 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    player_x = current_player
+    if valid_move?(index)
+      move(index, player_x)
+      display_board
+    else
+      turn
+    end
+  end
+  
+  def turn_count
     
   end
+  
 end
