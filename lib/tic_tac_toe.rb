25f2dbd601de14ac61e589a32be43d11fe1dee45
Erick Castille <ecastille924@gmail.com>
2020-09-29 23:17:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,5 +67,16 @@ WIN_COMBINATIONS = [
     turn_count % 2 == 0 ? "X" : "O"
   end
   
+  def won?
+    WIN_COMBINATIONS.detect do |win_combo|
+      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+        return win_combo
+      elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+        return win_combo
+      end
+        false
+    end
+  end
+  
   
 end
